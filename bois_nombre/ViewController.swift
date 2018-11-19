06e73d2f1d9edbdddde15c705f796be44c581b2f
BOIS Martin <boismart@mac06-m102.utt.fr>
2018-11-19 15:31:26 +0100

    Initial Commit

---
+++ bois_nombre/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bois_nombre
+//
+//  Created by BOIS Martin on 19/11/2018.
+//  Copyright © 2018 IF26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
